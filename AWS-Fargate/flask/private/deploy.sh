89727c2c40b7108c04994e6541c9c6d521b7e0e7
Yo-mah-Ya <shanzhongziye@gmail.com>
2024-10-20 19:03:32 -0400

    initial commit

---
+++ AWS-Fargate/flask/private/deploy.sh
@@ -0,0 +1,16 @@
+STACK_NAME="EcsOnFargate"
+
+aws cloudformation deploy \
+	--template-file template.yml \
+	--stack-name ${STACK_NAME} \
+	--parameter-overrides \
+	ProjectPrefix="" \
+	VpcId="" \
+	PublicSubnetAId="" \
+	PublicSubnetCId="" \
+	PrivateSubnetAId="" \
+	PrivateSubnetCId="" \
+	PrivateRouteTableAId="" \
+	PrivateRouteTableCId="" \
+	EcsAppContainerPort=3000 \
+	--capabilities CAPABILITY_NAMED_IAM
---
+++ AWS-Fargate/flask/public/do.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+profile='default'
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text --profile $profile)
+AWS_REGION=$(aws configure get region --profile $profile)
+APPLICATION_IMAGE_NAME="app:latest"
+
+REPOSITORY_BASE=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+SOURCE_DIR=$(cd $(dirname ${BASH_SOURCE:-0}) && pwd)
+cd ${SOURCE_DIR}
+
+case "$1" in
+"build") cd ${SOURCE_DIR}/app && docker image build --platform linux/amd64 -t ${APPLICATION_IMAGE_NAME} . ;;
+"exec") docker container exec -it $2 sh ;;
+"login") aws ecr get-login-password --region ${AWS_REGION} --profile $profile | docker login --username AWS --password-stdin ${REPOSITORY_BASE} ;;
+"push") docker image push ${REPOSITORY_BASE}/app:latest ;;
+"rmi") docker image ls -aq | xargs docker image rm -f ;;
+"tag") docker image tag ${APPLICATION_IMAGE_NAME} ${REPOSITORY_BASE}/app:latest ;;
+*) echo "pass me args" ;;
+esac
